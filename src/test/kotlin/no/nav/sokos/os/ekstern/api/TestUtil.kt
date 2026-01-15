739b4932c5e4afe5ac176fefa1257f00b48ed8bc
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2026-01-15 18:08:18 +0100

    add basic api tests

--- src/test/kotlin/no/nav/sokos/os/ekstern/api/TestUtil.kt
+++ src/test/kotlin/no/nav/sokos/os/ekstern/api/TestUtil.kt
@@ -3,5 +3,9 @@ package no.nav.sokos.os.ekstern.api
 internal const val API_BASE_PATH = "/api/v1"
 
 object TestUtil {
-    // Her ligger alt verktøy metoder som brukes i testene
+    fun String.readFromResource(): String =
+        TestUtil::class.java.classLoader
+            .getResource(this)
+            ?.readText()
+            ?: throw RuntimeException("Kunne ikke lese fil: $this")
 }
---
+++ src/test/kotlin/no/nav/sokos/os/ekstern/api/Testdata.kt
@@ -0,0 +1,181 @@
+package no.nav.sokos.os.ekstern.api
+
+import no.nav.sokos.os.ekstern.api.dto.annuler.KravgrunnlagAnnulerRequest
+import no.nav.sokos.os.ekstern.api.dto.annuler.KravgrunnlagAnnulerResponse
+import no.nav.sokos.os.ekstern.api.dto.detaljer.DetaljerPeriode
+import no.nav.sokos.os.ekstern.api.dto.detaljer.DetaljerPostering
+import no.nav.sokos.os.ekstern.api.dto.detaljer.HentKravgrunnlagDetaljerRequest
+import no.nav.sokos.os.ekstern.api.dto.detaljer.HentKravgrunnlagDetaljerResponse
+import no.nav.sokos.os.ekstern.api.dto.detaljer.KravgrunnlagDetaljer
+import no.nav.sokos.os.ekstern.api.dto.kravgrunnlag.HentKravgrunnlagRequest
+import no.nav.sokos.os.ekstern.api.dto.kravgrunnlag.HentKravgrunnlagResponse
+import no.nav.sokos.os.ekstern.api.dto.kravgrunnlag.Kravgrunnlag
+import no.nav.sokos.os.ekstern.api.dto.vedtak.Periode
+import no.nav.sokos.os.ekstern.api.dto.vedtak.Postering
+import no.nav.sokos.os.ekstern.api.dto.vedtak.TilbakekrevingsvedtakRequest
+import no.nav.sokos.os.ekstern.api.dto.vedtak.TilbakekrevingsvedtakResponse
+
+object Testdata {
+    val tilbakekrevingsvedtakRequest =
+        TilbakekrevingsvedtakRequest(
+            kodeAksjon = "NY",
+            vedtakId = 123456789L,
+            vedtaksDato = "2026-01-15",
+            kodeHjemmel = "FVL-22",
+            renterBeregnes = true,
+            enhetAnsvarlig = "8020",
+            kontrollfelt = "2026-01-15-01.12.34.123456",
+            saksbehandlerId = "Z999999",
+            datoTilleggsfrist = null,
+            perioder =
+                listOf(
+                    Periode(
+                        periodeFom = "2026-01-01",
+                        periodeTom = "2026-01-31",
+                        renterPeriodeBeregnes = true,
+                        belopRenter = 150.50,
+                        posteringer =
+                            listOf(
+                                Postering(
+                                    kodeKlasse = "KL_KODE_CLASS1",
+                                    belopOpprinneligUtbetalt = 10000.00,
+                                    belopNy = 8000.00,
+                                    belopTilbakekreves = 2000.00,
+                                    belopUinnkrevd = 0.00,
+                                    belopSkatt = 500.00,
+                                    kodeResultat = "FULL_TILBAKEKREV",
+                                    kodeAarsak = "FEIL_OPPLYSNINGER",
+                                    kodeSkyld = "BRUKER",
+                                ),
+                            ),
+                    ),
+                ),
+        )
+
+    val tilbakekrevingsvedtakResponse =
+        TilbakekrevingsvedtakResponse(
+            status = 200,
+            melding = "Vedtak sendt",
+            vedtakId = 123456789L,
+            datoVedtakFagsystem = "2026-01-15",
+        )
+
+    val hentKravgrunnlagRequest =
+        HentKravgrunnlagRequest(
+            kodeAksjon = "HENT",
+            gjelderId = "12345678901",
+            typeGjelder = "PERSON",
+            utbetalesTilId = "12345678901",
+            typeUtbetalesTil = "PERSON",
+            enhetAnsvarlig = "8020",
+            kodeFaggruppe = "PEN",
+            kodeFagomraade = "PENALD",
+            fagsystemId = "FS123456",
+            kravgrunnlagId = null,
+            saksbehandlerId = "Z999999",
+        )
+
+    val kravgrunnlag =
+        Kravgrunnlag(
+            kravgrunnlagId = 123456L,
+            kodeStatusKrav = "NY",
+            gjelderId = "12345678901",
+            typeGjelder = "PERSON",
+            utbetalesTilId = "12345678901",
+            typeUtbetalesTil = "PERSON",
+            kodeFagomraade = "PENALD",
+            fagsystemId = "FS123456",
+            datoVedtakFagsystem = "2026-01-15",
+            enhetBosted = "0219",
+            enhetAnsvarlig = "8020",
+            datoKravDannet = "2026-01-15",
+            datoPeriodeFom = "2026-01-01",
+            datoPeriodeTom = "2026-01-31",
+            belopSumFeilutbetalt = 10000.00,
+        )
+
+    val hentKravgrunnlagResponse =
+        HentKravgrunnlagResponse(
+            status = 200,
+            melding = "Kravgrunnlag hentet",
+            kravgrunnlagListe = listOf(kravgrunnlag),
+        )
+
+    val hentKravgrunnlagDetaljerRequest =
+        HentKravgrunnlagDetaljerRequest(
+            kodeAksjon = "HENT_DETALJER",
+            kravgrunnlagId = 123456L,
+            enhetAnsvarlig = "8020",
+            saksbehandlerId = "Z999999",
+        )
+
+    val kravgrunnlagDetaljer =
+        KravgrunnlagDetaljer(
+            kravgrunnlagId = 123456L,
+            vedtakId = 123456789L,
+            kodeStatusKrav = "NY",
+            kodeFagomraade = "PENALD",
+            fagsystemId = "FS123456",
+            datoVedtakFagsystem = "2026-01-15",
+            vedtakIdImgjort = 123456788L,
+            gjelderId = "12345678901",
+            typeGjelder = "PERSON",
+            utbetalesTilId = "12345678901",
+            typeUtbetalesTilId = "PERSON",
+            kodeHjemmel = "FVL-22",
+            renterBeregnes = true,
+            enhetAnsvarlig = "8020",
+            enhetBosted = "0219",
+            enhetBehandl = "8020",
+            kontrollfelt = "2026-01-15-01.12.34.123456",
+            saksbehandlerId = "Z999999",
+            referanse = "REF123456",
+            datoTilleggsfrist = null,
+            perioder =
+                listOf(
+                    DetaljerPeriode(
+                        periodeFom = "2026-01-01",
+                        periodeTom = "2026-01-31",
+                        belopSkattMnd = 500.00,
+                        posteringer =
+                            listOf(
+                                DetaljerPostering(
+                                    kodeKlasse = "KL_KODE_CLASS1",
+                                    typeKlasse = "YTEL",
+                                    belopOpprinneligUtbetalt = 10000.00,
+                                    belopNy = 8000.00,
+                                    belopTilbakekreves = 2000.00,
+                                    belopUinnkrevd = 0.00,
+                                    skattProsent = 25.0,
+                                    kodeResultat = "FULL_TILBAKEKREV",
+                                    kodeAarsak = "FEIL_OPPLYSNINGER",
+                                    kodeSkyld = "BRUKER",
+                                ),
+                            ),
+                    ),
+                ),
+        )
+
+    val hentKravgrunnlagDetaljerResponse =
+        HentKravgrunnlagDetaljerResponse(
+            status = 200,
+            melding = "Kravgrunnlag detaljer hentet",
+            kravgrunnlag = kravgrunnlagDetaljer,
+        )
+
+    val kravgrunnlagAnnulerRequest =
+        KravgrunnlagAnnulerRequest(
+            kodeAksjon = "ANNULER",
+            vedtakId = 123456789L,
+            enhetAnsvarlig = "8020",
+            saksbehandlerId = "Z999999",
+        )
+
+    val kravgrunnlagAnnulerResponse =
+        KravgrunnlagAnnulerResponse(
+            status = 200,
+            melding = "Kravgrunnlag annullert",
+            vedtakId = 123456789L,
+            saksbehandlerId = "Z999999",
+        )
+}
