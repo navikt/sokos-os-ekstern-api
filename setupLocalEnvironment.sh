63f8d87444f25f2ba4f9790680c11cd7774ad214
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2026-01-23 14:27:18 +0100

    minor adjustments to script based on shellcheck

--- setupLocalEnvironment.sh
+++ setupLocalEnvironment.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Ensure user is authenticated, and run login if not.
-gcloud auth print-identity-token &> /dev/null
-if [ $? -gt 0 ]; then
+if ! gcloud auth print-identity-token &> /dev/null; then
     gcloud auth login
 fi
+
 kubectl config use-context dev-gcp
 kubectl config set-context --current --namespace=okonomi
 
@@ -12,11 +12,11 @@ kubectl config set-context --current --namespace=okonomi
 POD_NAME=$(kubectl get pods | grep sokos-os-ekstern-api | cut -f1 -d' ')
 
 # Get AZURE system variables (excluding NAV_TRUSTSTORE_PATH)
-envValue=$(kubectl exec -it $POD_NAME -c sokos-os-ekstern-api -- env | egrep "^AZURE|NAV_TRUSTSTORE" | grep -v "NAV_TRUSTSTORE_PATH" | sort)
+envValue=$(kubectl exec -it "$POD_NAME" -c sokos-os-ekstern-api -- env | grep -E "^AZURE|NAV_TRUSTSTORE" | grep -v "NAV_TRUSTSTORE_PATH" | sort)
 
 # Extract truststore from pod to local directory
 echo "Copying truststore from pod..."
-kubectl cp $POD_NAME:/etc/ssl/certs/java/cacerts ./cacerts -c sokos-os-ekstern-api
+kubectl cp "$POD_NAME":/etc/ssl/certs/java/cacerts ./cacerts -c sokos-os-ekstern-api
 
 # Set AZURE as local environment variables
 rm -f defaults.properties
