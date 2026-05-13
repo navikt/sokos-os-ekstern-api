26cce5c24c414aa215ff5241f3c909064e208aae
bjornko <bjorn.vaggen.konestabo@nav.no>
2026-05-13 14:48:35 +0200

    ktlint endring.

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,12 +95,14 @@ configurations.all {
                 useVersion("4.2.13.Final")
                 because(
                     "CVE-2026-42587: Netty HttpContentDecompressor maxAllocation bypass with br/zstd/snappy leads to decompression bomb DoS. Affected version = 4.2.11.Final, patched in >= 4.2.13.Final",
-                )            }
+                )
+            }
             if (requested.group == "io.netty" && requested.name == "netty-codec-http2") {
                 useVersion("4.2.13.Final")
                 because(
                     "CVE-2026-42587: Netty HttpContentDecompressor maxAllocation bypass with br/zstd/snappy leads to decompression bomb DoS. Affected version = 4.2.11.Final, patched in >= 4.2.13.Final",
-                )            }
+                )
+            }
             if (requested.group == "org.bouncycastle" && requested.name == "bcprov-jdk18on") {
                 useVersion("1.84")
                 because("Bouncy Castle Has Covert Timing Channel Vulnerability. Affected version >= 1.71, < 1.84")
