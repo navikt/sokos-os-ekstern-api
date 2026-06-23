daef38c5864c7567386f3a10d45e946e1d78e5bf
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2026-06-23 14:08:14 +0200

    bumper diverse netty-bibliotek

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,21 +91,19 @@ configurations.all {
                 useVersion("2.21.1")
                 because("jackson-core: Number Length Constraint Bypass in Async Parser Leads to Potential DoS Condition. Affected version >= 2.19.0, < 2.21.1")
             }
-            if (requested.group == "io.netty" && requested.name == "netty-codec-http") {
-                useVersion("4.2.13.Final")
-                because(
-                    "CVE-2026-42587: Netty HttpContentDecompressor maxAllocation bypass with br/zstd/snappy leads to decompression bomb DoS. Affected version = 4.2.11.Final, patched in >= 4.2.13.Final",
-                )
-            }
             if (requested.group == "io.netty" && requested.name == "netty-codec-http2") {
-                useVersion("4.2.13.Final")
+                useVersion("4.2.15.Final")
                 because(
                     "CVE-2026-42587: Netty HttpContentDecompressor maxAllocation bypass with br/zstd/snappy leads to decompression bomb DoS. Affected version = 4.2.11.Final, patched in >= 4.2.13.Final",
                 )
             }
             if (requested.group == "io.netty" && requested.name == "netty-transport-native-epoll") {
-                useVersion("4.2.13.Final")
+                useVersion("4.2.15.Final")
                 because("CVE-2026-42577 >= 4.2.0.Alpha1, <= 4.2.12.Final")
+            }
+            if (requested.group == "io.netty" && requested.name == "netty-handler") {
+                useVersion("4.2.15.Final")
+                because("Netty has an IPv6 Subnet Filter Bypass via Incorrect Comparator Masking. Affected version >= 4.2.0.Alpha1, <= 4.2.12.Final")
             }
         }
     }
